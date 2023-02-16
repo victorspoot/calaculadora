b06d7c72856c88c991253bb6e90656bc396b1650
Victor Sá <v_h_p_s@hotmail.com>
2023-02-16 15:36:22 -0300

    começando o script

---
+++ script.js
@@ -0,0 +1,11 @@
+//Receber os dados = os numeros que serão digitados
+const visor = document.querySelector('#visor')
+const numeros = [...document.querySelectorAll('button.numero')]
+
+visor.innerHTML = `Olá`
+numeros[0].onclick = function limpaTela() {
+  visor.innerHTML = ''
+}
+
+
+console.log()

37ce0e172936008436126da088efbb3eb47581d3
Victor Sá <v_h_p_s@hotmail.com>
2023-02-16 22:43:51 -0300

    criei a div visor-container

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Receber os dados = os numeros que serão digitados
-const visor = document.querySelector('#visor')
+const visor = document.querySelector('#visor-calculadora')
 const numeros = [...document.querySelectorAll('button.numero')]
 
 visor.innerHTML = `Olá`

48c9e59cb989c39b9e487bc66d4963b0ab3f4cc9
Victor Sá <v_h_p_s@hotmail.com>
2023-02-16 23:36:42 -0300

    desenvolvendo a lógica do script

--- script.js
+++ script.js
@@ -2,10 +2,35 @@
 const visor = document.querySelector('#visor-calculadora')
 const numeros = [...document.querySelectorAll('button.numero')]
 
-visor.innerHTML = `Olá`
 numeros[0].onclick = function limpaTela() {
   visor.innerHTML = ''
+} 
+
+numeros[1].onclick = function dividir() {
+  numeros.splice(1, 1, 2)
+  visor.innerHTML += `/`
+}
+
+numeros[2].onclick = function imprimirSete() {
+  numeros[2] = 7
+  visor.innerHTML += `7`
+  return numeros
+}
+
+numeros[3].onclick = function imprimirOito() {
+  numeros[3] = 8
+  visor.innerHTML += `8`
+  return numeros
+}
+
+numeros[16].onclick = function resultado() {
+  if(numeros[1].onclick = true) {
+    let resultado = numeros / numeros
+    visor.innerHTML = `${resultado}`
+  }
 }
 
 
-console.log()
+
+
+console.log(numeros)

6904b3cc36ea09ef9f54648005f896b56623e6b2
Victor Sá <v_h_p_s@hotmail.com>
2023-02-17 11:56:39 -0300

    calculadora finalizada

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function insert(numero) {
   document.getElementById("visor-calculadora").innerHTML += numero
 }
+
+function limparTela() {
+  document.getElementById('visor-calculadora').innerHTML = ""
+}
+
+function apagar() {
+ let res = document.getElementById('visor-calculadora').innerHTML
+ document.getElementById('visor-calculadora').innerHTML = res.substring(0, res.length -1)
+}
+
+function calcular() {
+  let res = document.getElementById("visor-calculadora").innerHTML
+  if(res) {
+    document.getElementById('visor-calculadora').innerHTML = eval(res)
+  } 
+}

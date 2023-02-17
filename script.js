33ab343a8bfc76bc4cccc565d26e0c5642323cea
Victor Sá <v_h_p_s@hotmail.com>
2023-02-17 11:16:24 -0300

    css fixed

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-//Receber os dados = os numeros que serão digitados
-const visor = document.querySelector('#visor-calculadora')
-const numeros = [...document.querySelectorAll('button.numero')]
-
-numeros[0].onclick = function limpaTela() {
-  visor.innerHTML = ''
-} 
-
-numeros[1].onclick = function dividir() {
-  numeros.splice(1, 1, 2)
-  visor.innerHTML += `/`
+function insert(numero) {
+  document.getElementById("visor-calculadora").innerHTML += numero
 }
-
-numeros[2].onclick = function imprimirSete() {
-  numeros[2] = 7
-  visor.innerHTML += `7`
-  return numeros
-}
-
-numeros[3].onclick = function imprimirOito() {
-  numeros[3] = 8
-  visor.innerHTML += `8`
-  return numeros
-}
-
-numeros[16].onclick = function resultado() {
-  if(numeros[1].onclick = true) {
-    let resultado = numeros / numeros
-    visor.innerHTML = `${resultado}`
-  }
-}
-
-
-
-
-console.log(numeros)
